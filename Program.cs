cc59e274ddb30b8e5a670b51e76689dfb8254c99
Mithin Thomas <mithingthomas@gmail.com>
2023-06-25 17:25:16 -0400

    Resolve null variables

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.Write("What is your name? ");
-            string name = Console.ReadLine();
+            string? name = Console.ReadLine();
             Console.WriteLine($"Hello {name}");
         }
     }
