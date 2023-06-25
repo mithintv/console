a3229c6dc04aec95573b4ba80e7963ee06bd1d4e
Mithin Thomas <mithingthomas@gmail.com>
2023-06-25 17:24:23 -0400

    Reading input from the console

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
         }
     }
 }
