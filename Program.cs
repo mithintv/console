8a0ba1c25fee3e6168a6295e3995b9ab3d551584
Mithin Thomas <mithingthomas@gmail.com>
2023-06-28 12:37:23 -0400

    Classes

--- Program.cs
+++ Program.cs
@@ -7,23 +7,9 @@ namespace ConsoleApp
     public class Program
     {
 
-        static void PaintCar(CarColor cc)
-        {
-            Console.WriteLine("The car was painted {0} with the code {1}", cc, (int)cc);
-        }
-        enum CarColor : byte
-        {
-            Orange = 1,
-            Blue,
-            Green,
-            Red,
-            Yellow
-        }
         static void Main(string[] args)
         {
-            // ----- ENUM -----
-            CarColor car1 = CarColor.Blue;
-            PaintCar(car1);
+
         }
     }
 }
