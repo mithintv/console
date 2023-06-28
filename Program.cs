1d3337b3734a009f3f5672b08c22c19ff7bdbc6e
Mithin Thomas <mithingthomas@gmail.com>
2023-06-28 17:33:09 -0400

    Abstract Classes and Polymorphism

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using System.Globalization;
 using System.Text;
 
+// This time we cover Abstract Classes, Abstract Methods, Base Classes, Is, As, Casting and more about Polymorphism
+
 namespace ConsoleApp
 {
     public class Program
@@ -10,45 +12,37 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
 
-            Animal whiskers = new Animal()
+            // We can store our shapes in a Shape array as long as it contains subclasses of Shape
+            Shape[] shapes = { new Circle(5), new Rectangle(4, 5) };
+
+
+            // Cycle through shapes and print the area
+            foreach (Shape s in shapes)
             {
-                Name = "Whiskers",
-                Sound = "Meow"
-            };
-
-            Dog grover = new Dog()
-            {
-                Name = "Grover",
-                Sound = "Woof",
-                Sound2 = "Grrrr"
-            };
-
-            grover.Sound = "Wooooof";
+                s.GetInfo();
+                Console.WriteLine("{0} Area : {1:f2}", s.Name, s.Area());
 
 
-            whiskers.MakeSound();
-            grover.MakeSound();
-            whiskers.SetAnimalIDInfo(12345, "Sally Smith");
-            grover.SetAnimalIDInfo(12346, "Paul Brown");
+                // You can use as to check if an object is of a specific type
+                Circle testCirc = s as Circle;
+                if (testCirc == null)
+                {
+                    Console.WriteLine("This isn't a Circle");
+                }
 
-            whiskers.GetAnimalIDInfo();
-            Animal.AnimalHealth getHealth = new Animal.AnimalHealth();
-            Console.WriteLine("Is my animal healthy : {0}", getHealth.HealthyWeight(11, 46));
 
-            Animal monkey = new Animal()
-            {
-                Name = "Happy",
-                Sound = "Eeeee"
-            };
+                // You can use is to check the data type
+                if (s is Circle)
+                {
+                    Console.WriteLine("This isn't a Rectangle");
+                }
+            }
 
-            Animal spot = new Dog()
-            {
-                Name = "Spot",
-                Sound = "Wooooofff",
-                Sound2 = "Geeerrr"
-            };
+            // You can store any class as a base class and call the subclass methods even if they don't exist in the base class by casting
+            object circ1 = new Circle(4);
 
-            spot.MakeSound();
+            Circle circ2 = (Circle)circ1;
+            Console.WriteLine("The {0} Area is {1:f2}", circ2.Name, circ2.Area());
         }
     }
 }
---
+++ Shape.cs
@@ -0,0 +1,14 @@
+namespace ConsoleApp
+{
+    abstract class Shape
+    {
+        public string Name { get; set; }
+
+        public virtual void GetInfo()
+        {
+            Console.WriteLine($"This is a {Name}");
+        }
+
+        public abstract double Area();
+    }
+}
