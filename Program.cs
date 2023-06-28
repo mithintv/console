e71f7e0f41c4025cee34ec3ea738d2c897e74a51
Mithin Thomas <mithingthomas@gmail.com>
2023-06-28 10:15:51 -0400

    StringBuilder

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Text;
 
 namespace ConsoleApp
 {
@@ -27,30 +29,54 @@ namespace ConsoleApp
         // -------- END OF FUNCTIONS --------
         static void Main(string[] args)
         {
-            double num1 = 5;
-            double num2 = 0;
+            // ----- STRINGBUILDER -----
+            // Each time you change a string you are actually creating a new string which is inefficient when you are working with large blocks of text
+            // StringBuilders actually change the text rather then make a copy
 
-            try
-            {
-                Console.WriteLine("5 / 0 = {0}", DoDivision(num1, num2));
-            }
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine("You can't divide by zero");
-                Console.WriteLine(ex.GetType().Name);
-                Console.WriteLine(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occurred");
-                Console.WriteLine(ex.GetType());
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Cleaning Up");
-            }
+            // Create a StringBuilder with a default size
+            // of 16 characters, but it grows automatically
+            StringBuilder sb = new StringBuilder("Random text");
 
+            // Create a StringBuilder with a size of 256
+            StringBuilder sb2 = new StringBuilder("More stuff that is very important", 256);
+
+            // Get max size
+            Console.WriteLine("Capacity : {0}", sb2.Capacity);
+
+            // Get length
+            Console.WriteLine("Length : {0}", sb2.Length);
+
+            // Add text to StringBuilder
+            sb2.AppendLine("\nMore important text");
+
+            // Define culture specific formating
+            CultureInfo enUS = CultureInfo.CreateSpecificCulture("en-US");
+
+            // Append a format string
+            string bestCust = "Bob Smith";
+            sb2.AppendFormat(enUS, "Best Customer : {0}", bestCust);
+
+            // Output StringBuilder
+            Console.WriteLine(sb2.ToString());
+
+            // Replace a string
+            sb2.Replace("text", "characters");
+            Console.WriteLine(sb2.ToString());
+
+            // Clear a string builder
+            sb2.Clear();
+            sb2.Append("Random text");
+
+            // Are objects equal
+            Console.WriteLine(sb.Equals(sb2));
+
+            // Insert at an index
+            sb2.Insert(11, " that's great");
+            Console.WriteLine("Insert : {0}", sb2.ToString());
+
+            // Remove number of characters starting at index
+            sb2.Remove(11, 7);
+            Console.WriteLine("Remove : {0}", sb2.ToString());
         }
     }
 }
