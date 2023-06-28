031f55ae27f3c71ada4cec4b2f7a4fe25cb3a9fa
Mithin Thomas <mithingthomas@gmail.com>
2023-06-28 09:53:00 -0400

    Loops

--- Program.cs
+++ Program.cs
@@ -18,70 +18,31 @@ namespace ConsoleApp
         // -------- END OF FUNCTIONS --------
         static void Main(string[] args)
         {
-            // ----- IF / ELSE / -----
-            // Relational Operators : > < >= <= == !=
-            // Logical Operators : && || !
-
-            int age = 17;
-            if ((age >= 5) && (age <= 7))
+            int i = 1;
+            while (i <= 10)
             {
-                Console.WriteLine("Go to elementary school");
-            }
-            if ((age > 7) && (age < 13))
-            {
-                Console.WriteLine("Go to middle school");
-            }
-            if ((age > 13) && (age < 19))
-            {
-                Console.WriteLine("Go to high school");
-            }
-            else
-            {
-                Console.WriteLine("Go to college");
+                if (i % 2 == 0)
+                {
+                    i++;
+                    continue;
+                }
+                if (i == 9) break;
+                Console.WriteLine(i);
+                i++;
             }
 
-            if ((age < 14) || (age > 67))
+            Random rnd = new Random();
+            int secretNumber = rnd.Next(1, 11);
+            int numberGuessed = 0;
+            Console.WriteLine("Random Num : {0}", secretNumber);
+
+            do
             {
-                Console.WriteLine("You shouldn't work");
-            }
+                Console.WriteLine("Enter a number between 1 & 10 :");
+                numberGuessed = Convert.ToInt32(Console.ReadLine());
+            } while (secretNumber != numberGuessed);
 
-            Console.WriteLine("! true = " + (!true));
-
-            // Ternary Operator
-            // Assigns the 1st value if true and otherwise the 2nd
-            bool canDrive = age >= 16 ? true : false;
-
-            // Switch is used when you have limited options
-            // The only way to use ranges is to stack the possible values
-            switch (age)
-            {
-                case 1:
-                case 2:
-                    Console.WriteLine("Go to Day Care");
-                    break;
-                case 3:
-                case 4:
-                    Console.WriteLine("Go to Preschool");
-                    break;
-                case 5:
-                    Console.WriteLine("Go to Kindergarten");
-                    break;
-                default:
-                    Console.WriteLine("Go to another school");
-                    // You can also jump out of a switch with goto
-                    goto OtherSchool;
-            }
-
-        OtherSchool:
-            Console.WriteLine("Elementary, Middle, High School");
-
-            // To compare strings use Equals
-            string name2 = "Derek";
-            string name3 = "Derek";
-            if (name2.Equals(name3, StringComparison.Ordinal))
-            {
-                Console.WriteLine("Names are equal");
-            }
+            Console.WriteLine("You guessed it! It was {0}", secretNumber);
 
         }
     }
