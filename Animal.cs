72197f505f0a91982044497e309d5fd329fc66ba
Mithin Thomas <mithingthomas@gmail.com>
2023-06-28 15:45:12 -0400

    Getters/Setters and Accessors

--- Animal.cs
+++ Animal.cs
@@ -4,38 +4,38 @@ namespace ConsoleApp
 {
     class Animal
     {
-        // Attributes (fields) that all Animals have public means can be directly changed after an object has been created
-        public string name;
-        public string sound;
+        // Define fields that are protected with private
+        // Private fields can only be accessed by methods in the class and they are not accessible by subclasses.
+        // Protected fields can only be accessed by methods in the class and by subclasses
+        private string name;
+        private string sound;
 
-        // static fields and methods belong to the class. A static field has the same value for all objects of the Animal type
-        static int numOfAnimals = 0;
+
+        // You can define constants
+        public const string SHELTER = "Derek's Home for Animals";
+
+        // You can define read-only fields that are set at runtime in constructors, but then can't be changed
+        public readonly int idNum;
 
 
-        // A constructor sets default values for fields when an object is created
-        // This is the default constructor if no parameters are sent
-        public Animal()
+        // Default constructor
+        public Animal() : this("No Name", "No Sound") { }
+
+        // Constructor called if only name is passed this passes the parameters to the next constructor
+        public Animal(string name) : this(name, "No Sound") { }
+
+        // Constructor that receives parameters
+        public Animal(string name, string sound)
         {
-            name = "No Name";
-            sound = "No Sound";
-            numOfAnimals++;
-        }
+            SetName(name);
+            Sound = sound;
 
+            // Increment the number of animals property
+            NumOfAnimals += 1;
 
-
-        // You can create additonal constructors but since we are definig defaults you don't have to
-        public Animal(string name = "No Name")
-        {
-            this.name = name;
-            this.sound = "No Sound";
-            numOfAnimals++;
-        }
-
-        public Animal(string name = "No Name", string sound = "No Sound")
-        {
-            this.name = name;
-            this.sound = sound;
-            numOfAnimals++;
+            // Define the read-only value which is the same for all Animals
+            Random rnd = new Random();
+            idNum = rnd.Next(1, 2147483640);
         }
 
         // Capabilities (methods) that all Animals have
@@ -44,9 +44,62 @@ namespace ConsoleApp
             Console.WriteLine("{0} says {1}", name, sound);
         }
 
-        public static int GetNumAnimals()
+
+        // Setters (Mutators) protect the fields from receiving bad data
+        public void SetName(string name)
         {
-            return numOfAnimals;
+            // Checks if any character in the string is a number and if so returns true
+            // Since we won't allow numbers we will protect our data
+            if (!name.Any(char.IsDigit))
+            {
+                this.name = name;
+            }
+            else
+            {
+                this.name = "No Name";
+                Console.WriteLine("Name can't contain numbers");
+            }
+        }
+
+
+        // Getters (Accessors) can provide output aside from the value stored
+        public string GetName()
+        {
+            return name;
+        }
+
+
+        // The preferred way to define getters and setters is through properties
+        public string Sound
+        {
+            get { return sound; }
+            set
+            {
+                // value is assigned the value passed in
+                if (value.Length > 10)
+                {
+                    sound = "No Sound";
+                    Console.WriteLine("Sound is too long");
+                }
+                else
+                {
+                    sound = value;
+                }
+            }
+        }
+
+
+        // You can have the getters and setters generated for you like this and also set the default value
+        public string Owner { get; set; } = "No Owner";
+
+
+        // You can also define static properties
+        public static int numOfAnimals = 0;
+
+        public static int NumOfAnimals
+        {
+            get { return numOfAnimals; }
+            set { numOfAnimals += value; }
         }
     }
 }
