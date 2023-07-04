873114e55e836785085c633c071825a2bae59275
Mithin <mithingthomas@gmail.com>
2023-07-04 15:05:08 -0400

    Serialization

---
+++ Animal.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.Serialization;
+
+
+// With serialization you can store the state  of an object in a file stream, pass it to a remote network
+
+namespace ConsoleApp
+{
+    // Defines that you want to serialize this class
+    [Serializable()]
+    public class Animal : ISerializable
+    {
+        public string? Name { get; set; }
+        public double? Weight { get; set; }
+        public double? Height { get; set; }
+        public int? AnimalID { get; set; }
+
+        public Animal() { }
+
+        public Animal(
+            string name = "No Name",
+            double weight = 0,
+            double height = 0,
+            int animalID = 0)
+        {
+            Name = name;
+            Weight = weight;
+            Height = height;
+            AnimalID = animalID;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{Name} weighs {Weight} lbs and is {Height} inches tall");
+        }
+
+        // Serialization function (Stores Object Data in File)
+        // SerializationInfo holds the key value pairs
+        // StreamingContext can hold additional info but we aren't using it here
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            // Assign key value pair for your data
+            info.AddValue("Name", Name);
+            info.AddValue("Weight", Weight);
+            info.AddValue("Height", Height);
+            info.AddValue("AnimalID", AnimalID);
+        }
+
+        // The deserialize function (Removes Object Data from File)
+        public Animal(SerializationInfo info, StreamingContext context)
+        {
+            // Get the values from info and assign them to the properties
+            Name = (string?)info.GetValue("Name", typeof(string));
+            Weight = (double?)info.GetValue("Weight", typeof(double));
+            Height = (double?)info.GetValue("Height", typeof(double));
+            AnimalID = (int?)info.GetValue("AnimalID", typeof(int));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,40 +1,90 @@
-﻿namespace ConsoleApp
+﻿using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace ConsoleApp
 {
     public class Program
     {
+
         static void Main(string[] args)
         {
-            // ----- BINARYWRITER / BINARYREADER -----
-            // Used to read and write data types 
-            string textFilePath = @"C:\Users\mithi\c#data\testfile4.txt";
+            Animal? bowser = new Animal("Bowser", 45, 25, 1);
 
-            // Create file and retrieve it
-            FileInfo datFile = new FileInfo(textFilePath);
+            // Serialize the object data to a file
+            Stream stream = File.Open(@"C:\Users\mithi\c#data\AnimalData.dat", FileMode.Create);
 
-            // Open the file
-            BinaryWriter bw = new BinaryWriter(datFile.OpenWrite());
+            // Send the object data to the file
 
-            // Data to save to the file
-            string randText = "Random Text";
-            int myAge = 47;
-            double height = 6.25;
+            // Binary Serialization is no longer supported
+            //BinaryFormatter bf = new BinaryFormatter();
+            //bf.Serialize(stream, bowser);
 
-            // Write data to a file
-            bw.Write(randText);
-            bw.Write(myAge);
-            bw.Write(height);
+            JsonSerializer.Serialize(stream, bowser);
+            stream.Close();
 
-            bw.Close();
+            // Delete the bowser data
+            bowser = null;
 
-            // Open file for reading
-            BinaryReader br = new BinaryReader(datFile.OpenRead());
+            // Read object data from the file
+            stream = File.Open(@"C:\Users\mithi\c#data\AnimalData.dat", FileMode.Open);
 
-            // Output data
-            Console.WriteLine(br.ReadString());
-            Console.WriteLine(br.ReadInt32());
-            Console.WriteLine(br.ReadDouble());
+            // Binary Serialization is no longer supported
+            //bf = new BinaryFormatter();
+            //bowser = (Animal)bf.Deserialize(stream);
 
-            br.Close();
+            bowser = JsonSerializer.Deserialize<Animal>(stream);
+            stream.Close();
+
+            Console.WriteLine(bowser?.ToString());
+
+            bowser!.Weight = 50;
+
+            // XmlSerializer writes object data as XML. Make sure you have a c#data folder
+            XmlSerializer serializer = new XmlSerializer(typeof(Animal));
+            using (TextWriter tw = new StreamWriter(@"C:\Users\mithi\c#data\bowser.xml"))
+            {
+                serializer.Serialize(tw, bowser);
+            };
+
+
+            // Delete bowser data
+            bowser = null;
+
+            // Deserialize from XML to the object
+            XmlSerializer deserializer = new XmlSerializer(typeof(Animal));
+            TextReader? reader = new StreamReader(@"C:\Users\mithi\c#data\bowser.xml");
+            object? obj = deserializer.Deserialize(reader);
+            bowser = (Animal?)obj;
+
+            Console.WriteLine(bowser?.ToString());
+
+
+            // Save a collection of Animals
+            List<Animal>? theAnimals = new List<Animal> {
+            new Animal("Mario", 60, 30, 2),
+            new Animal("Luigi", 55, 24, 3),
+            new Animal("Peach", 40, 20, 4),
+            };
+
+            using (Stream fs = new FileStream(@"C:\Users\mithi\c#data\animals.xml", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                XmlSerializer serializer2 = new XmlSerializer(typeof(List<Animal>));
+                serializer2.Serialize(fs, theAnimals);
+            };
+
+            // Delete list data
+            theAnimals = null;
+
+            // Read data from XML
+            XmlSerializer? deserializer2 = new XmlSerializer(typeof(List<Animal>));
+            using (FileStream? fs2 = File.OpenRead(@"C:\Users\mithi\c#data\animals.xml"))
+            {
+                theAnimals = (List<Animal>?)deserializer2.Deserialize(fs2);
+            };
+            foreach (Animal a in theAnimals!)
+            {
+                Console.WriteLine(a.ToString());
+            }
         }
     }
 }
