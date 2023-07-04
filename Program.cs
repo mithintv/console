628bcac1dbf421b3ded6ef195c9092214b978d41
Mithin <mithingthomas@gmail.com>
2023-07-04 13:05:14 -0400

    Directory Functions & File I/O

--- Program.cs
+++ Program.cs
@@ -1,35 +1,80 @@
-﻿using System;
-using System.Globalization;
-using System.Text;
-using System.Collections;
-using System.Collections.Generic;
-
-namespace ConsoleApp
+﻿namespace ConsoleApp
 {
-    // With threads you can execute multiple pieces of code that share resources and data without corrupting it
-
-    // You can't guarantee when a thread executes. You also must lock resources until a thread is done with them or you could corrupt them
     public class Program
     {
-        // ----- Passing Data to Threads -----
-        // You can pass arguments to a thread using a lambda expression
-
-        static void CountTo(int maxNum, int threadNum)
-        {
-            for (int i = 0; i <= maxNum; i++)
-            {
-                Console.WriteLine($"{threadNum} :  {i}");
-            }
-        }
         static void Main(string[] args)
         {
-            Thread t = new Thread(() => CountTo(10, 0));
-            t.Start();
+            // ----- MESSING WITH DIRECTORIES -----
 
-            new Thread(() =>
+            // Get access to the current directory
+            DirectoryInfo currDir = new DirectoryInfo(".");
+
+            // Get access to a directory with a path
+            DirectoryInfo customDir = new DirectoryInfo(@"C:/Users/mithi");
+
+            // Get the directory path
+            Console.WriteLine(currDir.FullName);
+            Console.WriteLine(customDir.FullName);
+
+            // Get the directory name
+            Console.WriteLine(customDir.Name);
+
+            // Get the parent directory
+            Console.WriteLine(customDir.Parent);
+
+            // What type is it
+            Console.WriteLine(customDir.Attributes);
+
+            // When was it created
+            Console.WriteLine(customDir.CreationTime);
+
+            // Create a directory
+            DirectoryInfo dataDirectory = new DirectoryInfo(@"C:/Users/mithi/c#data");
+            dataDirectory.Create();
+
+            // Delete a directory
+            //Directory.Delete(@"C:/Users/mithi/c#data\");
+
+
+            // ----- SIMPLE FILE READING & WRITING -----
+
+            // Write a string array to a text file
+            string[] customers =
             {
-                CountTo(5, 1);
-            }).Start();
+                "Bob Smith",
+                "Sally Smith",
+                "Robert Smith"
+            };
+            string textFilePath = @"C:\Users\mithi\c#data\textfile1.txt";
+
+            // Write the array
+            File.WriteAllLines(textFilePath, customers);
+
+            // Read strings from array
+            foreach (string customer in File.ReadAllLines(textFilePath))
+            {
+                Console.WriteLine($"Customer : {customer}");
+            }
+
+
+            // ----- GETTING FILE DATA -----
+
+            DirectoryInfo myDataDir = new DirectoryInfo(@"C:/Users/mithi/c#data");
+
+            // Get all txt files 
+            FileInfo[] txtFiles = myDataDir.GetFiles("*.txt", SearchOption.AllDirectories);
+
+            // Number of matches
+            Console.WriteLine($"Matches : {txtFiles.Length}");
+
+            foreach (FileInfo file in txtFiles)
+            {
+                // Get file name
+                Console.WriteLine(file.Name);
+
+                // Get bytes in file
+                Console.WriteLine(file.Length);
+            }
         }
     }
 }
