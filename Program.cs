5a37fe71cfdd52ff839070eae917ccd50e6ff205
Mithin Thomas <mithingthomas@gmail.com>
2023-07-04 10:57:52 -0400

    Threads Part 1

--- Program.cs
+++ Program.cs
@@ -6,224 +6,28 @@ using System.Collections.Generic;
 
 namespace ConsoleApp
 {
-    // Language Integrated Query (LINQ) provides many tools for working with data. LINQ is similar to SQL, but it can work with data aside from databases. You manipulate data using Query Expressions
+    // With threads you can execute multiple pieces of code that share resources and data without corrupting it
+
+    // You can't guarantee when a thread executes. You also must lock resources until a thread is done with them or you could corrupt them
     public class Program
     {
-
-        static int[] QueryIntArray()
+        // ----- Simple Thread Example -----
+        static void Print1()
         {
-            int[] nums = { 5, 10, 15, 20, 25, 30, 35 };
-
-            var gt20 = from num in nums
-                       where num > 20
-                       orderby num
-                       select num;
-
-            foreach (int num in gt20)
+            for (int i = 0; i < 1000; i++)
             {
-                Console.WriteLine(num);
-            }
-            Console.WriteLine($"Get Type : {gt20.GetType()}");
-
-            var listGT20 = gt20.ToList<int>();
-            var arrayGT20 = gt20.ToArray();
-
-            nums[0] = 40;
-            foreach (int num in gt20)
-            {
-                Console.WriteLine(num);
-            }
-            return arrayGT20;
-        }
-        static void QueryStringArray()
-        {
-            string[] dogs = { "K9", "Brian Griffin", "Scooby Doo", "Old Yeller", "Rin Tin Tin", "Benji", "Charlie B. Barkin", "Lassie", "Snoopy" };
-
-            // Get strings with spaces and put in alphabetical order
-            // from states where data comes from and where to store each piece as you cycle
-            // where defines conditions that must be met
-            // orderby defines what data is used for ordering results (ascending / descending)
-            // select defines the variable that is checked against the condition
-            var dogSpaces = from dog in dogs
-                            where dog.Contains(" ")
-                            orderby dog descending
-                            select dog;
-
-            foreach (var i in dogSpaces)
-            {
-                Console.WriteLine(i);
-            }
-
-
-            int[] intArray = QueryIntArray();
-            foreach (int num in intArray)
-            {
-                Console.WriteLine(num);
+                Console.Write(1);
             }
         }
-        static void QueryArrayList()
-        {
-            ArrayList famAnimals = new ArrayList()
-            {
-                new Animal {
-                    Name = "Heidi",
-                    Height = .8,
-                    Weight = 18 },
-                new Animal {
-                    Name = "Shrek",
-                    Height = 4,
-                    Weight = 130 },
-                new Animal {
-                    Name = "Congo",
-                    Height = 3.8,
-                    Weight = 90 }
-            };
-
-            // You have to convert the ArrayList into an IEnumerable
-            var famAnimalEnum = famAnimals.OfType<Animal>();
-
-            var smAnimals = from animal in famAnimalEnum
-                            where animal.Weight <= 90
-                            orderby animal.Name
-                            select animal;
-            foreach (var animal in smAnimals)
-            {
-                Console.WriteLine($"{animal.Name} weighs {animal.Weight}lbs");
-            }
-        }
-        static void QueryCollection()
-        {
-            ArrayList animalList = new ArrayList()
-            {
-                new Animal {
-                    Name = "German Shepard",
-                    Height = 25,
-                    Weight = 77 },
-                new Animal {
-                    Name = "Chihuahua",
-                    Height = 7,
-                    Weight = 4.4 },
-                new Animal {
-                    Name = "Saint Bernard",
-                    Height = 30,
-                    Weight = 200 }
-            };
-            var animalListEnum = animalList.OfType<Animal>();
-            var bigDogs = from dog in animalListEnum
-                          where (dog.Weight > 70) && (dog.Height > 25)
-                          orderby dog.Name
-                          select dog;
-
-            foreach (var animal in bigDogs)
-            {
-                Console.WriteLine($"{animal.Name} weight {animal.Weight}lbs");
-            }
-
-        }
-        static void QueryAnimalData()
-        {
-            Animal[] animals = new[]
-            {
-                new Animal {
-                    Name = "German Shepard",
-                    Height = 25,
-                    Weight = 77,
-                    AnimalID = 1 },
-                new Animal {
-                    Name = "Chihuahua",
-                    Height = 7,
-                    Weight = 4.4,
-                    AnimalID = 2 },
-                new Animal {
-                    Name = "Saint Bernard",
-                    Height = 30,
-                    Weight = 200,
-                    AnimalID = 3 },
-                new Animal {
-                    Name = "Pug",
-                    Height = 12,
-                    Weight = 6,
-                    AnimalID = 1 },
-                new Animal {
-                    Name = "Beagle",
-                    Height = 15,
-                    Weight = 23,
-                    AnimalID = 2 }
-            };
-            Owner[] owners = new[]{
-                new Owner{
-                    Name = "Doug Parks",
-                    OwnerID = 1 },
-                new Owner{
-                    Name = "Sally Smith",
-                    OwnerID = 2},
-                new Owner{
-                    Name = "Paul Brooks",
-                    OwnerID = 3}
-            };
-
-            // Extract name and height
-            var nameHeight = from a in animals
-                             select new
-                             {
-                                 a.Name,
-                                 a.Height
-                             };
-
-            // Convert to an object array
-            Array arrNameHeight = nameHeight.ToArray();
-            foreach (var i in arrNameHeight)
-            {
-                Console.WriteLine(i.ToString());
-            }
-
-            // Create an inner join
-            // Join info in owners and animals using equal values for IDs
-            // Store values for animal and owner
-            var innerJoin = from animal in animals
-                            join owner in owners
-                            on animal.AnimalID equals owner.OwnerID
-                            select new { OwnerName = owner.Name, AnimalName = animal.Name };
-
-            foreach (var i in innerJoin)
-            {
-                Console.WriteLine($"{i}");
-            }
-
-            // Create a group inner join
-            // Get all animals and put them in a newly created owner group if their IDs match the owners ID
-            var groupJoin = from owner in owners
-                            orderby owner.OwnerID
-                            join animal in animals
-                            on owner.OwnerID equals animal.AnimalID
-                            into ownerAnimals
-                            select new
-                            {
-                                Owner = owner.Name,
-                                Animals = from ownerAnimal in ownerAnimals
-                                          orderby ownerAnimal.Name
-                                          select ownerAnimal
-                            };
-            int totalAnimals = 0;
-
-            foreach (var ownerGroup in groupJoin)
-            {
-                Console.WriteLine(ownerGroup.Owner);
-                foreach (var animal in ownerGroup.Animals)
-                {
-                    totalAnimals++;
-                    Console.WriteLine($"* {animal.Name}");
-                }
-            }
-        }
-
         static void Main(string[] args)
         {
-            QueryIntArray();
-            QueryStringArray();
-            QueryArrayList();
-            QueryCollection();
-            QueryAnimalData();
+            Thread t = new Thread(Print1);
+            t.Start();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(0);
+            }
         }
     }
 }
