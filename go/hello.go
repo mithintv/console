f2ae8e439c84187d358fb527cc6b063867e351db
Mithin Thomas <mithingthomas@gmail.com>
2024-05-06 11:57:09 -0400

    basic types

--- go/hello.go
+++ go/hello.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+
+	// initialize variables here
+	var smsSendingLimit int
+	var costPerSMS float32
+	var hasPermission bool
+	var username string
+
+	fmt.Printf("%v %f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
 }
